14a96ae627b05a839011dd17f1e6660571eb23b0
devemindset <menwomenchat@gmail.com>
2025-10-15 22:17:35 +0200

    add id in admin product

--- back_e/products/admin.py
+++ back_e/products/admin.py
@@ -17,7 +17,7 @@ from .models import (
 # -----------------------------
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "description",)
+    list_display = ("id","name", "description",)
     search_fields = ("name", "description")
 
 
